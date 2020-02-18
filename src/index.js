0c0967e4ae3d501c45d5a365e78408c105c01a27
Shymmy Garcia <shymmy.garcia-ext@block.one>
2020-02-18 11:05:00 -0500

    Add subscribers

--- src/index.js
+++ src/index.js
@@ -1,37 +1,67 @@
+const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
+// // 1
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+// let links = [{
+//   id: 'link-0',
+//   url: 'www.howtographql.com',
+//   description: 'Fullstack tutorial for GraphQL'
+// }]
 
-// 2
-let idCount = links.length
+// // 2
+// let idCount = links.length
+// const resolvers = {
+//   Query: {
+//     info: () => `This is the API of a Hackernews Clone`,
+//     feed: (root, args, context, info) => {
+//       return context.prisma.links()
+//     },
+//   },
+//   Mutation: {
+//     post: (root, args, context) => {
+//       return context.prisma.createLink({
+//         url: args.url,
+//         description: args.description,
+//       })
+//     },
+//   },
+// }
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-  },
-  Mutation: {
-    // 2
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
-  },
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
 }
-
 // 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
+
+/*
+  "data": {
+    "signup": {
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazZxd3E0bzZwbDEwMGIwMDhlZmVoemt0IiwiaWF0IjoxNTgxOTcxMDYzfQ.EhJ2Z-UWYFOsqNJFln5DZetvEbh5ws-vKRzBWTisPXI",
+      "user": {
+        "id": "ck6qwq4o6pl100b008efehzkt"
+      }
+    }
+  }
+}
+*/
